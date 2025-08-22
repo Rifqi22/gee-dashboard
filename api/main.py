705231ccdc4cdbfd5b4cf9fb1a4954f626cc95c4
Rifqi22 <rifqinaufalfayyadh@gmail.com>
2025-08-22 14:33:45 +0700

    API: improve logic at /tiles add validation for date argument using regex add try except for error handler

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import ee
+import re
 
 # Authenticate and Initialize Earth Engine
 ee.Authenticate()
@@ -15,14 +16,22 @@ def root():
 # get the tiles image from the given date ex: 2025-01
 @app.get("/tiles/{date}")
 def get_tile(date: str):
-    # call the modis data then filter it based on the date
-    dataset = ee.ImageCollection("MODIS/061/MOD11A2") \
-        .filterDate(f"{date}-01", f"{date}-28") \
-        .mean().select("LST_Day_1km").multiply(0.02).subtract(273.15) #calculate the average of LST and convert it into Celcius (default Kelvin)
+    # validate the date using regex
+    if not re.match(r"^\d{4}-\d{2}$", date):
+        raise HTTPException(status_code=400, detail="Date must be in YYYY-MM format")
+    
+    try:
+        # call the modis data then filter it based on the date
+        dataset = ee.ImageCollection("MODIS/061/MOD11A2") \
+            .filterDate(f"{date}-01", f"{date}-28") \
+            .mean().select("LST_Day_1km").multiply(0.02).subtract(273.15) #calculate the average of LST and convert it into Celcius (default Kelvin)
 
-    vis_params = {"min": 0, "max": 40, "palette": ["blue", "green", "yellow", "red"]}
-    map_id = dataset.getMapId(vis_params)
+        vis_params = {"min": 0, "max": 40, "palette": ["blue", "green", "yellow", "red"]}
+        map_id = dataset.getMapId(vis_params)
 
-    # Return the tile url 
-    # ex :  https://earthengine.googleapis.com/v1/projects/gee-dashboard-project/maps/b4adff9e71301b7e68645f7bf356ec49-6ac08859643006d0a476aaa1bfd657d3/tiles/{z}/{x}/{y}
-    return {"tile_url": map_id["tile_fetcher"].url_format}
+        # Return the tile url 
+        # This url is a ready used for the front end, it contain all the metadata that it needs like the id, param vis etc
+        # ex :  https://earthengine.googleapis.com/v1/projects/gee-dashboard-project/maps/b4adff9e71301b7e68645f7bf356ec49-6ac08859643006d0a476aaa1bfd657d3/tiles/{z}/{x}/{y}
+        return {"tile_url": map_id["tile_fetcher"].url_format}
+    except ee.EEException as e:
+         raise HTTPException(status_code=500, detail=f"Earth Engine error: {str(e)}")
