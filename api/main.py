dbf7b02a9cde440da93ed3b6e127ed9b4f4894f6
Rifqi22 <rifqinaufalfayyadh@gmail.com>
2025-08-22 14:20:18 +0700

    API: create a simple route /tiles to get the MODIS satellite and its url

--- api/main.py
+++ api/main.py
@@ -11,3 +11,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "GEE Dashboard Backend is running"}
+
+# get the tiles image from the given date ex: 2025-01
+@app.get("/tiles/{date}")
+def get_tile(date: str):
+    # call the modis data then filter it based on the date
+    dataset = ee.ImageCollection("MODIS/061/MOD11A2") \
+        .filterDate(f"{date}-01", f"{date}-28") \
+        .mean().select("LST_Day_1km").multiply(0.02).subtract(273.15) #calculate the average of LST and convert it into Celcius (default Kelvin)
+
+    vis_params = {"min": 0, "max": 40, "palette": ["blue", "green", "yellow", "red"]}
+    map_id = dataset.getMapId(vis_params)
+
+    # Return the tile url 
+    # ex :  https://earthengine.googleapis.com/v1/projects/gee-dashboard-project/maps/b4adff9e71301b7e68645f7bf356ec49-6ac08859643006d0a476aaa1bfd657d3/tiles/{z}/{x}/{y}
+    return {"tile_url": map_id["tile_fetcher"].url_format}
